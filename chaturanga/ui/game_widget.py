60150ffb35b37793961427cb675ff9cb0ceeee53
Andre Fernandes <fpf.andre@gmail.com>
2022-07-17 10:40:04 -0300

    Mostra jogador atual e não mostra lances para jogador errado

--- chaturanga/ui/game_widget.py
+++ chaturanga/ui/game_widget.py
@@ -1,7 +1,7 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
-from ui.board_widget import BoardWidget
+from chaturanga.ui.board_widget import BoardWidget
 from chaturanga.game import Game
 from chaturanga.pieces.piece import Color
 from chaturanga.errors import InvalidMovement, InvalidPosition
@@ -14,9 +14,9 @@ class GameWidget(QWidget):
         self.last_piece = None
 
         self.board_widget = BoardWidget(self)
-        self.player_label = QLabel('Jogam as Brancas.')
-        self.info_label = QLabel('Nada de errado, apenas jogue.')
-        self.restart_button = QPushButton('Começar um novo jogo')
+        self.player_label = QLabel("Jogam as Brancas.")
+        self.info_label = QLabel("Nada de errado, apenas jogue.")
+        self.restart_button = QPushButton("Começar um novo jogo")
 
         self.restart_button.clicked.connect(self.restart_game)
         menu_layout = QVBoxLayout()
@@ -43,7 +43,7 @@ class GameWidget(QWidget):
         self.last_piece = None
         self.board_widget.clear_highlights()
         self.board_widget.update_board()
-        self.info_label.setText('')
+        self.info_label.setText("")
 
     def square_clicked_callback(self, row, col):
         clicked_square = self.game.board.get_square(row, col)
@@ -55,29 +55,32 @@ class GameWidget(QWidget):
 
     def choose_piece(self, target):
         if target.piece is None:
-            self.info_label.setText('Quadrado vazio')
+            self.info_label.setText("")
+            return
+
+        if target.piece.color != self.game.current_color():
+            self.info_label.setText("Escolha uma peça da sua cor")
             return
 
         self.last_piece = target.piece
         self.board_widget.clear_highlights()
         self.board_widget.highlight_square_red(target.row, target.col)
         self.board_widget.highlight_movements(target.row, target.col)
-        self.info_label.setText('')
-    
+        self.info_label.setText("")
+
     def move_choosen_piece(self, target):
         origin = self.last_piece.position
 
         try:
             origin = self.last_piece.position
             self.game.make_movement(origin, target)
-            self.info_label.setText('')
+            self.info_label.setText("")
 
         except InvalidMovement as error:
             self.info_label.setText(str(error))
-        
+
         except InvalidPosition as error:
             self.info_label.setText(str(error))
-
 
         self.last_piece = None
         self.board_widget.clear_highlights()
@@ -85,8 +88,15 @@ class GameWidget(QWidget):
 
         if self.game.match_finished():
             self.show_winner()
+        else:
+            if self.game.current_color() == Color.WHITE:
+                self.player_label.setText("Jogam as Brancas.")
+            elif self.game.current_color() == Color.BLACK:
+                self.player_label.setText("Jogam as Pretas.")
+            else:
+                raise ValueError("Unknown current player.")
 
-    def show_winner(self):       
+    def show_winner(self):
         msg = QMessageBox()
         msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
 
@@ -95,9 +105,9 @@ class GameWidget(QWidget):
         elif self.game.get_winner() == Color.BLACK:
             msg.setText("VITÓRIA DAS PRETAS!")
         else:
-            return 
+            return
 
-        msg.setInformativeText('Deseja iniciar uma nova partida?')
+        msg.setInformativeText("Deseja iniciar uma nova partida?")
         msg.setWindowTitle("Vitória!")
         ret = msg.exec()
 
